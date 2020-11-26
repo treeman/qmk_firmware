9618e1b5c596c4ccbd3fd7e1504ed28ca982f2bb
Pete Sevander <pete.sevander@gmail.com>
2020-11-26 14:50:30 +0200

    6 col wrappers

--- users/sevanteri/wrappers.h
+++ users/sevanteri/wrappers.h
@@ -9,19 +9,31 @@
 #define _________________QWERTY_L1_________________   MY_Q, MY_W, MY_E, MY_R, MY_T
 #define _________________QWERTY_L2_________________   MY_A, MY_S, MY_D, MY_F, MY_G
 #define _________________QWERTY_L3_________________   MY_Z, MY_X, MY_C, MY_V, MY_B
+#define _QWERTY_L1_______________   KC_TAB,  _________________QWERTY_L1_________________
+#define _QWERTY_L2_______________   CTRLESC, _________________QWERTY_L2_________________
+#define _QWERTY_L3_______________   KC_LSFT, _________________QWERTY_L3_________________
 
 #define _________________QWERTY_R1_________________   MY_Y, MY_U, MY_I,    MY_O,   MY_P
 #define _________________QWERTY_R2_________________   MY_H, MY_J, MY_K,    MY_L,   MY_OE
 #define _________________QWERTY_R3_________________   MY_N, MY_M, MY_COMM, MY_DOT, MY_MINS
+#define _______________QWERTY_R1_   _________________QWERTY_R1_________________, FI_QUOT
+#define _______________QWERTY_R2_   _________________QWERTY_R2_________________, MY_AE
+#define _______________QWERTY_R3_   _________________QWERTY_R3_________________, KC_RSFT
 
 
 #define _________________SYMB_L1___________________   KC_1,       KC_2,       KC_3,       KC_4,       KC_5
 #define _________________SYMB_L2___________________   LSFT(KC_1), LSFT(KC_2), LSFT(KC_3), LSFT(KC_4), LSFT(KC_5)
 #define _________________SYMB_L3___________________   _______,    RALT(KC_2), RALT(KC_3), RALT(KC_4), RALT(KC_5)
+#define  _SYMB_L1_________________  LSFT(FI_SECT),  _________________SYMB_L1___________________
+#define  _SYMB_L2_________________  FI_SECT,        _________________SYMB_L1___________________
+#define  _SYMB_L3_________________  RALT(FI_SECT),  _________________SYMB_L3___________________
 
 #define _________________SYMB_R1___________________   KC_6,       KC_7,       KC_8,       KC_9,       KC_0
 #define _________________SYMB_R2___________________   LSFT(KC_6), LSFT(KC_7), LSFT(KC_8), LSFT(KC_9), LSFT(KC_0)
 #define _________________SYMB_R3___________________   _______,    RALT(KC_7), RALT(KC_8), RALT(KC_9), RALT(KC_0)
+#define _________________SYMB_R1_   _________________SYMB_R1___________________, LSFT(FI_PLUS)
+#define _________________SYMB_R2_   _________________SYMB_R1___________________, FI_PLUS
+#define _________________SYMB_R3_   _________________SYMB_R3___________________, RALT(FI_PLUS)
 
 
 #define _________________FUNCL_L1__________________   KC_F1, KC_F2,  KC_F3,  KC_F4,  _______
